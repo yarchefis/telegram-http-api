fc7715573a4d9687e65935cff1c2feb7390e9ac6
Yaroslav Fisyukov <76122377+yarchefis@users.noreply.github.com>
2024-04-05 17:53:36 +0300

    добавлена функция отравки сообщения

--- main.py
+++ main.py
@@ -4,17 +4,42 @@ from telethon.sync import TelegramClient
 from telethon.tl.types import User
 import json
 import config
+import urllib.parse
 
 api_id = config.api_id
 api_hash = config.api_hash
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        parsed_path = urllib.parse.urlparse(self.path)
+        path_components = parsed_path.path.split('/')
+        
         if self.path == '/':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b'All OK')
+
+        elif path_components[1] == 'api' and path_components[2] == 'chat' and len(path_components) == 5 and path_components[4] == 'msg':
+            # New route /api/chat/<id>/msg?text=<тут_текст>
+            chat_id = path_components[3]
+            query_components = urllib.parse.parse_qs(parsed_path.query)
+            if 'text' in query_components:
+                message_text = query_components['text'][0]
+
+                with TelegramClient('session_name', api_id, api_hash) as client:
+                    client.send_message(int(chat_id), message_text)
+                
+                self.send_response(200)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(b'Message sent successfully')
+
+            else:
+                self.send_response(400)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(b'Missing text parameter in query')
 
         elif self.path == '/api/chats':
             self.send_response(200)
