f861fdf5f0506b5f9c246ab562d10ff7217428c2
Yaroslav Fisyukov <76122377+yarchefis@users.noreply.github.com>
2024-04-18 20:27:39 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,6 +16,9 @@ pip install telethon --break-system-packages
 git clone https://github.com/yarchefis/telegram-http-api
 cd telegram-http-api
 
+clear
+echo "посетите сайт https://my.telegram.org/auth и создайте свое приложения а затем указжите данные."
+
 # Запрос и вставка api_id и api_hash в файл config.py
 echo "Введите api_id:"
 read api_id
@@ -26,11 +29,11 @@ read api_hash
 sed -i "s/api_id = ''/api_id = '$api_id'/" config.py
 sed -i "s/api_hash = ''/api_hash = '$api_hash'/" config.py
 
-cd
+cd ~/
 
 # Создание файла start.sh
 echo "#!/bin/bash" > start.sh
 echo "python3 main.py" >> start.sh
 chmod +x start.sh
 
-echo "Установка завершена. Теперь запустите start.sh для запуска сервера."
+echo "Установка завершена. Теперь запустите ./start.sh для запуска сервера."
